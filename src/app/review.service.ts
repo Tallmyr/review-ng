b8b8a1311e69d6c14b5a3acc5d8aecc10b61cca1
Simon Tallmyr <simon@tallmyr.se>
2021-03-13 02:47:24 +0000

    Update to new API structure

--- src/app/review.service.ts
+++ src/app/review.service.ts
@@ -4,16 +4,19 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
-const localUrl = 'http://localhost:8000/reviews';
+const localUrl = 'http://localhost:8000/reviews/';
 
-export interface ReviewShort {
-  id: string;
-  name: string;
-  user: string;
-  short: string;
-  stars: number;
+export interface ReviewTags {
+  string
 }
-
+export interface ReviewShort {
+  name: string;
+  text: string;
+  stars: number;
+  user: string;
+  id: string;
+  tags: ReviewTags;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -21,7 +24,6 @@ export interface ReviewShort {
 export class ReviewService {
   constructor(private http: HttpClient) {}
   getReviews() {
-    return this.http.get<ReviewShort>('http://127.0.0.1:8000/reviews');
-    
+    return this.http.get<ReviewShort>(localUrl);
   }
 }
