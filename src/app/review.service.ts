a0b5a3a7c7002b14e2bc7a2951c16757f1aa776e
Simon Tallmyr <simon@tallmyr.se>
2021-03-13 15:13:46 +0000

    Deleted unneeded services

--- src/app/review.service.ts
+++ src/app/review.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
@@ -9,21 +9,15 @@ const localUrl = 'http://localhost:8000/reviews/';
 export interface ReviewTags {
   string
 }
-export interface ReviewShort {
-  name: string;
-  text: string;
-  stars: number;
-  user: string;
-  id: string;
-  tags: ReviewTags;
-}
-
 @Injectable({
   providedIn: 'root',
 })
 export class ReviewService {
   constructor(private http: HttpClient) {}
   getReviews() {
-    return this.http.get<ReviewShort>(localUrl);
+    return this.http.get(localUrl);
+  }
+  getReview(id: string) {
+    return this.http.get(localUrl + id);
   }
 }
